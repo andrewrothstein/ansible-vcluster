a2e687c11fefafb99f1afee2d5d42a4dc377499d
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-09-05 21:49:05 -0400

    vcluster:0.11.2

---
+++ README.md
@@ -0,0 +1,40 @@
+andrewrothstein.vcluster
+=========
+
+![Build Status](https://github.com/andrewrothstein/ansible-vcluster/actions/workflows/build.yml/badge.svg)
+
+Installs the [vcluster](https://www.vcluster.com/) cli.
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.vcluster
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksums.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+MIRROR=https://github.com/loft-sh/vcluster/releases/download
+
+# https://github.com/loft-sh/vcluster/releases/download/v0.11.2/vcluster-linux-amd64.sha256
+
+dl()
+{
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local dotexe=${4:-""}
+    local platform="${os}-${arch}"
+    local url=$MIRROR/v${ver}/vcluster-${platform}${dotexe}.sha256
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(curl -sSLf $url)
+}
+
+dl_ver() {
+    local ver=$1
+    printf "  '%s':\n" $ver
+    dl $ver darwin amd64
+    dl $ver darwin arm64
+    dl $ver linux amd64
+    dl $ver linux arm64
+    dl $ver linux 386
+    dl $ver windows amd64 .exe
+    dl $ver windows 386 .exe
+}
+
+dl_ver ${1:-0.11.2}
